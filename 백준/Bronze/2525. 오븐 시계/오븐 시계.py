771f2f4cb4f32dddf684587eb3e351c924f3ddf4
Lina <100856824+LinaYeom@users.noreply.github.com>
2022-06-26 06:21:22 +0900

    [Bronze III] Title: 오븐 시계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,20 @@
+A, B = map(int,input().split())
+C = int(input())
+D = None
+
+if (0<=C<1001):
+    D = C//60
+    if (0<D):
+        A += D
+        B = B+(C-D * 60)
+    else : 
+        B += C 
+        
+if(59<B):
+    A += 1
+    B -=60
+
+if(23<A):
+    A -= 24
+
+print(A, B)
