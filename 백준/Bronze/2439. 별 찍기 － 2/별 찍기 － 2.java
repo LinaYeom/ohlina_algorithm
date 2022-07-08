4d7974a793230ddeaeddf1daed732c9cd4c0695b
Lina <100856824+LinaYeom@users.noreply.github.com>
2022-07-09 06:37:22 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 352 ms, Memory: 18308 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -6,13 +6,15 @@ public class Main {
 		int n = sc.nextInt();
 
 		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n-i; j++) {
+			for (int j = 1; j <= n - i; j++) {
 				System.out.print(" ");
 			}
-			for (int k = 0; k < i; k++) {
+			for (int k = 1; k <= i; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+
 	}
+
 }
