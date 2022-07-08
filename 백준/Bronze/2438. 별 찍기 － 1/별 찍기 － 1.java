03b0f1cab5256f9ca7fc69b68c1acd91c0266ab7
Lina <100856824+LinaYeom@users.noreply.github.com>
2022-07-09 06:21:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 312 ms, Memory: 18060 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class Main {
  
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		in.close();
- 
-		for (int i = 1; i <= N; i++) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	
+		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
@@ -16,4 +14,3 @@ public class Main {
 		}
 	}
 }
- 
